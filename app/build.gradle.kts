dc966ecb6c6a8d003d74554762422a7008b5f325
MIUX-TECH <cepmuin98@gmail.com>
2026-09-13 22:59:48 +0700

    Configure Kotlin JVM target with AGP 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
@@ -38,6 +41,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
-
     implementation("com.github.mwiede:jsch:2.28.6")
 }
