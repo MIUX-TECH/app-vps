d9a5143b2fd9bcb7290f5880b9d6133ea060e540
MIUX-TECH <cepmuin98@gmail.com>
2026-09-13 23:02:33 +0700

    Raise compile SDK to 36 for current AndroidX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.botdash.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.botdash.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "1.1"
     }
